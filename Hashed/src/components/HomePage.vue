9660db4b07f7cf69f91f3c1119204af767d1bdf2
tkrzewina <59062750+tkrzewina@users.noreply.github.com>
2021-02-08 16:22:41 +0000

    commitonmaster

--- Hashed/src/components/HomePage.vue
+++ Hashed/src/components/HomePage.vue
@@ -1,23 +1,20 @@
 <template>
-    <div class="container-fluid m-0 p-0">
+    <div class="container-fluid m-0 p-0"> 
 
-        <div id="home-page" class="bg-danger" p-4>
-
+        <div id="home-page" class="full-hight bg-danger p-4">
         </div>
 
-        <div id="home-page" class="bg-danger" p-4>
-
+        <div id="about" class="bg-warning p-4">
         </div>
 
-        <div id="home-page" class="bg-danger" p-4>
-
+        <div id="home-page" class="bg-info p-4">
         </div>
 
-        <div id="home-page" class="bg-danger" p-4>
+        <div id="home-page" class="bg-dark p-4">
+        </div>                
 
-        </div>
- 
     </div> 
+
 </template>
 
 <script>
